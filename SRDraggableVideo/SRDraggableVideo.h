f004871ab4a31d35b5ae63d7790888997ba5a356
SRoller <sroller@justfab.com>
2019-06-24 11:46:58 -0400

    Initial Commit

---
+++ SRDraggableVideo/SRDraggableVideo.h
@@ -0,0 +1,19 @@
+//
+//  SRDraggableVideo.h
+//  SRDraggableVideo
+//
+//  Created by Shawn Roller on 6/24/19.
+//  Copyright © 2019 Shawn Roller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SRDraggableVideo.
+FOUNDATION_EXPORT double SRDraggableVideoVersionNumber;
+
+//! Project version string for SRDraggableVideo.
+FOUNDATION_EXPORT const unsigned char SRDraggableVideoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRDraggableVideo/PublicHeader.h>
+
+
